b83d727c7ab88cceb9df3af721bdbadf28ddf934
Ayush Bansal <ayushb6732@gmail.com>
2023-01-14 22:53:57 +0530

    Adding warranty generation blockchain functionality

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -3,10 +3,15 @@ import React, { useState, useLayoutEffect } from "react";
 import { useParams } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { ReactSession } from "react-client-session";
+import minting from "../utils";
+import { toast } from "react-hot-toast";
 
 const Results = () => {
+  ReactSession.setStoreType("localStorage");
   const [result, setResult] = useState([]);
   const [wrong, setWrong] = useState(true);
+  const [connected, setConnected] = useState(ReactSession.get("loggedIn"));
   const { query } = useParams();
 
   useLayoutEffect(() => {
@@ -15,10 +20,19 @@ const Results = () => {
         `http://localhost:5000/product/${query}`
       );
       setResult(response.data);
-      if(response.data.length == 0)
-        setWrong(false);
+      if (response.data.length == 0) setWrong(false);
     })();
   }, [query]);
+
+  const mint = async (e) => {
+    e.preventDefault();
+    const addr = result.map((item) => {
+      return item.manufacturer[0].wallet;
+    });
+    const nft = minting(addr[0], document.querySelector('#expiry').value);
+    console.log(nft);
+    toast.success('Minted NFT!');
+  };
 
   return (
     <section>
@@ -26,7 +40,9 @@ const Results = () => {
       <div className="min-h-screen bg-base-200">
         {!wrong && (
           <div className="flex justify-center pt-10">
-            <span className="text-4xl fw-bold">No results found for {query} !</span>
+            <span className="text-4xl fw-bold">
+              No results found for {query} !
+            </span>
           </div>
         )}
         {wrong && (
@@ -34,7 +50,6 @@ const Results = () => {
             {result.map((item) => {
               return (
                 <div className="card w-96 bg-base-100 shadow-xl" key={item._id}>
-                  {console.log(item)}
                   <figure>
                     <img
                       src={item.imgUrl}
@@ -52,7 +67,14 @@ const Results = () => {
                     </h2>
                     <p>{item.details}</p>
                     <div className="card-actions justify-end">
-                      <button className="btn btn-outline btn-warning">Issue warranty!</button>
+                      <button
+                        className="btn btn-outline btn-warning"
+                        disabled={!connected ? true : false}
+                      >
+                        <label htmlFor="my-modal">
+                        Issue warranty!
+                        </label>
+                      </button>
                     </div>
                   </div>
                 </div>
@@ -62,6 +84,30 @@ const Results = () => {
         )}
       </div>
       <Footer />
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box relative">
+          <label
+            htmlFor="my-modal"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="text-lg font-bold">
+            Enter warranty period in days!
+          </h3>
+          <div>
+            <form onSubmit={mint}>
+              <div className="form-group m-5 flex justify-center">
+              <input type="number" placeholder="365" id="expiry" className="input input-bordered input-accent w-full max-w-xs" />
+              </div>
+              <div className="form-group m-5 flex justify-center">
+                <button className="btn btn-outline btn-info" type="submit">Mint NFT!</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,21 @@
 import abi from './WarrantyToken.json'
 import { ethers } from 'ethers'
 
-const minting = async () => {
-    const contract = new ethers.Contract('0x5FbDB2315678afecb367f032d93F642f64180aa3', abi.abi);
+const createProduct = async (company, expiry) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner();
+    
 }
+
+const minting = async (company, expiry) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract('0x5FbDB2315678afecb367f032d93F642f64180aa3', abi.abi, signer);
+    console.log(contract);
+    const warranty = contract.Warranty()
+    const nft = await contract.mint(company, expiry);
+    console.log(nft);
+    return nft;
+}
+
+export default minting;
